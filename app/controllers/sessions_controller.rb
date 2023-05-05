15cac2e3c7eb4b1c1e3fabbc30e4216e8d7a3bdb
Omaredu <omaredu.02@gmail.com>
2023-05-05 14:08:42 -0600

    Added log_in and sign_up endpoints

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user&.valid_password?(session_params[:password])
+      render json: { status: 'User signed in successfully' }, status: :ok
+    else
+      render json: { errors: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  scope '/api' do
+    scope '/users' do
+      post '/sign_in', to: 'sessions#create'
+      post '/sign_up', to: 'users#create'
+    end
+  end
+
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
